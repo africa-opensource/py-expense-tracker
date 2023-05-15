f9454923faa39ab40e0cf0853829a46688927435
Cletus Tindan <cletustindan1@gmail.com>
2023-05-15 14:06:39 +0000

    Update on the Add expenses option

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 project_name = "Ey Expense Tracker"
 underline = "================================================"
 
@@ -19,9 +20,14 @@ def add_expense():
     #Request for the amount of each of the expenses
     for i in range(num_of_expenses):
                 amount = float(input("Enter the amount of expense #{} in GHS: " .format(i + 1)))
+                description = input("Enter category of expenses: ")
+                date  = input("Enter date in this form dd/mm/yyyy: ")
+                print(underline)
                 data.append(amount)
-    print(underline)
-    print("The list of amount recorded is given as: ", data,"\nThe sum of your expenses is: GHS",sum(data))
+                with open("expenses.txt", mode='w', newline='') as file:
+                    writer = csv.writer(file) 
+                    writer.writerow([description,amount,date])
+    print("Expenses added successfully and the total sum is: GHS",sum(data))
     print(underline)
 def main():
         print(project_name)
